9c7263c5a439b3ef807cefcc42a892d44c8b04d6
Jankin_Wu <2938567281@qq.com>
2025-12-19 03:34:23 +0800

    chore(version): Update the app version to 1.0.14

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 val osName = System.getProperty("os.name").lowercase()
 val osArch = System.getProperty("os.arch").lowercase()
 
-val appVersion = "1.0.13"
+val appVersion = "1.0.14"
 val appVersionSuffix = "Alpha"
 
 val platformStr = when {
